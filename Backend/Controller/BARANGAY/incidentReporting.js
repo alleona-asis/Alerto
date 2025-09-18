b5159cd4dbb90888c3ea879e733de31fa7d15e76
alleona-asis <adasis@gbox.adnu.edu.ph>
2025-09-18 17:32:38 +0800

    update2

--- Backend/Controller/BARANGAY/incidentReporting.js
+++ Backend/Controller/BARANGAY/incidentReporting.js
@@ -406,7 +406,6 @@ const updateReportStatus = async (req, res) => {
 // ==========================
 // const LAN_IP = process.env.LAN_IP || "192.168.1.2"; 
  const PORT = process.env.PORT || 5000;
- app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // const BASE_URL = `http://${LAN_IP}:${PORT}/uploads/proof`;
 
 /* 
